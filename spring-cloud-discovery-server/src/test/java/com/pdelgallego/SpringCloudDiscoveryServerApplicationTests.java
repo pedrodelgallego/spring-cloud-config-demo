279a5f5412c853fe8ac1275d20cd16348222493b
Pedro Gallego <pedro.delgallego@gmail.com>
2016-02-29 04:17:55 +0100

    add sentence example

---
+++ spring-cloud-discovery-server/src/test/java/com/pdelgallego/SpringCloudDiscoveryServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pdelgallego;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringCloudDiscoveryServerApplication.class)
+public class SpringCloudDiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
